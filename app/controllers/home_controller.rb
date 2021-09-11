678f15f61e91f391739ef8af2d89d3abf7c76004
akymdayo <akkuydmi@gmail.com>
2021-09-11 17:17:10 +0900

    Update application

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,11 @@
 class HomeController < ApplicationController
   before_action :logged_in_user, only:[:edit, :update, :destroy]
   def top
-    @schedules = Schedule.all
-    @user_schedules = Schedule.joins(:user_schedules).select("schedules.*, user_schedules.*").order("schedules.start_at")
-    @users = User.all
     @user_id = session[:user_id]
     @admin_flg = session[:admin_flg]
+    @schedules = Schedule.all.order("schedules.start_at")
+    @user_schedules = Schedule.joins(:user_schedules).select("schedules.*, user_schedules.*").order("schedules.start_at")
+    # @user_schedules_per = Schedule.left_joins(:user_schedules).select("schedules.*, user_schedules.*").order("schedules.start_at").where(user_schedules: { user_id: @user_id })
+    @users = User.all
   end
 end
--- app/controllers/tennis_conreoller_controller.rb
+++ app/controllers/tennis_conreoller_controller.rb
@@ -1,9 +1,11 @@
 class TennisConreollerController < ApplicationController
   def new
-      @schedules = Schedule.new
+      @schedules = Schedule.all.order("schedules.start_at")
       @user_schedules = Schedule.joins(:user_schedules).select("schedules.*, user_schedules.*").order("schedules.start_at")
+      # @user_schedules_per = Schedule.joins(:user_schedules).select("schedules.*, user_schedules.*").where(user_schedules: { user_id: 10 }).order("schedules.start_at")
       # @user_names = UserSchedule.joins(:users).select("user_schedules.*, users.*")
       @users = User.all
+      @schedules_add = Schedule.count + 1
   end
 
   #スケジュール登録
@@ -15,14 +17,14 @@ class TennisConreollerController < ApplicationController
       # スケジュール登録
       if @schedules.save
           # ユーザ毎のスケジュール情報登録
-          @users = User.all
-          @users.each do |users|
-            @user_schedules = UserSchedule.new()
-            @user_schedules.user_id = users.id
-            @user_schedules.schedule_id = @schedules.id
-            @user_schedules.join_flg = false
-            @user_schedules.save
-          end
+          # @users = User.all
+          # @users.each do |users|
+          #   @user_schedules = UserSchedule.new()
+          #   @user_schedules.user_id = users.id
+          #   @user_schedules.schedule_id = @schedules.id
+          #   @user_schedules.join_flg = false
+          #   @user_schedules.save
+          # end
           redirect_to root_url
       else
           redirect_to root_url
@@ -34,18 +36,27 @@ class TennisConreollerController < ApplicationController
       # ログ
       logger.debug(user_schedule_params)
 
-      @user_schedules = UserSchedule.find(params.require(:user_schedule)[:id])
-      # スケジュール登録
-      if @user_schedules.update(user_schedule_params)
-          redirect_to root_url
+      @user_schedules = UserSchedule.find_by(id:params.require(:user_schedule)[:id])
+      if @user_schedules == nil
+            # 個人スケジュール登録
+            @user_schedules = UserSchedule.new(insert_user_schedule_params)
+            if @user_schedules.save
+                redirect_to root_url
+            else
+                redirect_to root_url
+            end
       else
-          redirect_to root_url
+            # 個人スケジュール更新
+            if @user_schedules.update(user_schedule_params)
+                redirect_to root_url
+            else
+                redirect_to root_url
+            end
       end
   end
 
   #ユーザー作成
   def create_user
-      binding.pry
       @users = User.new(user_params)
       # スケジュール登録
       if @users.save
@@ -62,6 +73,10 @@ class TennisConreollerController < ApplicationController
 
       def user_schedule_params
           params.require(:user_schedule).permit(:id, :user_id, :schedule_id, :join_flg)
+      end
+
+      def insert_user_schedule_params
+          params.require(:user_schedule).permit(:user_id, :schedule_id, :join_flg)
       end
 
       def user_params
